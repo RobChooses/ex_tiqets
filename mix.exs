ffdd1c0a149d0bb883dfcf0062738e9dd58ff848
Robert Chu <RobChooses@users.noreply.github.com>
2019-11-08 21:33:38 +0000

    Add ExDoc

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule ExTiqets.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -20,7 +22,23 @@ defmodule ExTiqets.MixProject do
   defp deps do
     [
       {:tesla, "~> 1.3.0"},
-      {:jason, ">= 1.0.0"}
+      {:jason, ">= 1.0.0"},
+      {:ex_doc, "~> 0.21.0", only: :dev}
+    ]
+  end
+
+  defp description do
+    """
+    Elixir wrapper for the Tiqets V2 API]
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "config", "mix.exs", "README", "LICENSE"],
+      maintainers: ["Rob Chu"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/RobChooses/ex_tiqets"}
     ]
   end
 end
